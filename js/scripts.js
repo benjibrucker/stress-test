842a01da830c74182bbeacbf64fc54bbe1946931
BenjiBrucker <benjamin.brucker@gmail.com>
2021-02-03 22:44:52 -0600

    update more questions, add JS
    Co-authored-by: Nick Hennessy <njhnny@gmail.com>
    Co-authored-by: Faisal Rana <inquisitive@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,24 @@
 $(document).ready(function(){
   $("form#stress-test-survey").submit(function(event){
     event.preventDefault();
-    $("#negative-response").show();
+    
+    var arrayNCheckBoxes= [];
+    $("input:checkbox[name=negative]:checked").each(function(){
+      const negativeCheckBoxes = $(this).val();
+      $("#response1").append(negativeCheckBoxes + "<br>");
+      $("#response2").append(negativeCheckBoxes + "<br>");
+      $("#response3").append(negativeCheckBoxes + "<br>");
+      arrayNCheckBoxes.push(negativeCheckBoxes);
+      arrayNCheckBoxesL = arrayNCheckBoxes.length;
+      if (arrayNCheckBoxesL <= 2) {
+        $("#response1").show();
+      } else if (arrayNCheckboxesL === 3) {
+        $("#response2").show();
+      } else { 
+        (arrayNCheckBoxesL > 3); 
+        $("#response3").show();
+      }
+    });
+    
   });
 });
